15e9bddbf51fe1eda9961d885ec112d24fe376eb
onurk <onurk@DESKTOP-7FVNJAI>
2022-12-09 11:46:51 +0300

    Add project files.

---
+++ 09.12.2022_Uygulama/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace _09._12._2022_Uygulama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbbBilgi = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(555, 39);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Oyunu Başlat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "1 ile 100 arasında bir sayı giriniz";
+            // 
+            // lbbBilgi
+            // 
+            this.lbbBilgi.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbbBilgi.Location = new System.Drawing.Point(16, 234);
+            this.lbbBilgi.Name = "lbbBilgi";
+            this.lbbBilgi.Size = new System.Drawing.Size(551, 39);
+            this.lbbBilgi.TabIndex = 2;
+            this.lbbBilgi.Click += new System.EventHandler(this.lbbBilgi_Click);
+            // 
+            // label3
+            // 
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Location = new System.Drawing.Point(16, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(551, 40);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(308, 91);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 302);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(551, 37);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(12, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(555, 39);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Tahmin Et";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(579, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbbBilgi);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbbBilgi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 09.12.2022_Uygulama/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _09._12._2022_Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public int _sayi;
+        public int _Random;
+        public int _Count = 0;
+        public int _Count1 = 0;
+        public bool _dene;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        // 1.Görev oyunu başlata bastığımda timer çalışsın
+        // 2. görec rast gele bir sayı üretsin
+        // 3. görev dorm yüklendiğinde progressbar max60  btntahminet.enebiled=> faşse
+        // 4. oynu başlata bastığımda timer çalışsın
+        // 5. görev progressbar.value == progresbar.max labelları temizleyin
+        // 6. görev Tahmin et butonuna bastığımda kullanıcı sayı girdimi kontrol girmedi yada yanlış girdiyse uyarı verin
+        // 7. görev girilen sayıyı lbl bilgiye yazdır.
+        // 8. görev kullanıcının girdiği sayi ile randomdan gelen sayı arasındaki ilişkiyi kontrol edin lblBilgiye surumları atın 
+        // 9. görev kullanıcının tahmin ettiği sayı doğru ise kullanıcının ettiği süreye göre lblMesaj a bilegiler yazdıralım (%. tahminde bildin - 15. saniyede bildin diye)
+        //10.görev Timer ı tetikleyin(tick) progressbar.value değerini kontrol edin.(value =>0 başaramadın.)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Maximum = 60;
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random _RND = new Random();
+            _Random = _RND.Next(1,101);
+
+            _Count = 0;
+            _Count1 = 0;
+
+            timer1.Start(); // zamanlayıcı başladı
+            lbbBilgi.Text = ""; // text temizlendi
+            
+            // bar maksimum değerine geri sayabilmesi için ayarlandı
+            progressBar1.Value = progressBar1.Maximum; 
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _Count1++;
+            if(progressBar1.Value > progressBar1.Minimum)
+            {
+                progressBar1.Value = progressBar1.Maximum - _Count1;
+            }
+            if (_Count > 59)
+            {
+                label3.Text = "süre bitti başaramadın";
+                button2.Enabled = false;
+                timer1.Stop();
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _dene = int.TryParse(textBox1.Text, out _sayi);
+            button2.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(_dene != false && _sayi < 101 && _Count1 < 101)
+            {
+                _Count++;
+                if (_sayi < _Random)
+                {
+                    lbbBilgi.Text = "daha büyük bir sayıya ihtiyacın var";
+                }
+                else if (_sayi > _Random)
+                {
+                    lbbBilgi.Text = "daha küçük bir sayıya ihtiyacın var";
+                }
+                else if (_sayi == _Random)
+                {
+                    lbbBilgi.Text = "Tebrikler";
+                    label3.Text = _Count.ToString() + " . denemde buldunuz ve " + _Count1.ToString() + " snde tammamladınız";
+                    timer1.Stop();
+                    
+                }
+            }
+            else
+            {
+                lbbBilgi.Text = "Hatalşı giriş yaptınız";
+            }
+
+            
+        }
+     
+    }
+}
